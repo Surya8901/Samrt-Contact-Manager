939f753db3a00a4867fef5022aec7b9ce9cf4213
suraj <surajkumarbal45@gmail.com>
2023-06-29 13:58:13 +0530

    3rd Commit

--- SmartContactManager/src/main/java/com/smart/controller/HomeController.java
+++ SmartContactManager/src/main/java/com/smart/controller/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 	
-	@RequestMapping("/home")
+	@RequestMapping("/")
 	public String home(Model model) 
 	{
 		
@@ -21,5 +21,13 @@ public class HomeController {
 		
 		model.addAttribute("title","About - Smart Contact MAnager");
 		return "about";
+	}
+	
+	@RequestMapping("/signup")
+	public String signup(Model model) 
+	{
+		
+		model.addAttribute("title","Register - Smart Contact MAnager");
+		return "signup";
 	}
 }
